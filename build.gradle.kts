596a6ba19d0038295897c8508165a4eb5d5399a0
asyncant <git@asyncant.com>
2021-12-19 23:49:55 +0100

    Prepare v0.8.1 development.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
   kotlin("multiplatform") version "1.5.31" apply false
 }
 group = "com.asyncant.aws.lambda"
-version = "0.8"
+version = "0.8.1-SNAPSHOT"
 
 allprojects {
   repositories {
