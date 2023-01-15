e6dba4ec95a1866ec2d3c5e0f9633d51992eba85
asyncant <git@asyncant.com>
2023-01-15 16:59:42 +0100

    Release version 0.8.2.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
   kotlin("multiplatform") version "1.8.0" apply false
 }
 group = "com.asyncant.aws.lambda"
-version = "0.8.2-SNAPSHOT"
+version = "0.8.2"
 
 allprojects {
   tasks.withType<Test> {
