1c029f79b2b2c6e2c15580f60fd7e65f59c9d6ef
asyncant <git@asyncant.com>
2023-01-15 17:23:30 +0100

    Prepare v0.8.3 development.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
   kotlin("multiplatform") version "1.8.0" apply false
 }
 group = "com.asyncant.aws.lambda"
-version = "0.8.2"
+version = "0.8.3-SNAPSHOT"
 
 allprojects {
   tasks.withType<Test> {
