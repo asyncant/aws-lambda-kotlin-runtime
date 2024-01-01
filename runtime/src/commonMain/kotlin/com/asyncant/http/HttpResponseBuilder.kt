95fbb48c1e6d8d76c13ce6c84645ac957199b7c4
asyncant <git@asyncant.com>
2024-01-01 22:43:16 +0100

    Add chunked transfer-encoding support.
    * Used by lambda for streaming requests/responses, for example when invoked by an SNS topic.

--- runtime/src/commonMain/kotlin/com/asyncant/http/HttpResponseBuilder.kt
+++ runtime/src/commonMain/kotlin/com/asyncant/http/HttpResponseBuilder.kt
@@ -1,53 +1,125 @@
 package com.asyncant.http
 
+import com.asyncant.http.HttpResponseBuilder.HttpResponseState.BODY
+import com.asyncant.http.HttpResponseBuilder.HttpResponseState.CHUNKS
+import com.asyncant.http.HttpResponseBuilder.HttpResponseState.DONE
+import com.asyncant.http.HttpResponseBuilder.HttpResponseState.HEADER
 import com.asyncant.io.MessageReceiver
 
 internal class HttpResponseBuilder : MessageReceiver {
+  private var state: HttpResponseState = HEADER
   private var statusCode: Int = -1
   private var dataBuffer = ByteArray(16384)
   private var dataSize: Int = 0
-  private lateinit var headers: Map<String, List<String>>
   private var contentLength: Int = -1
+  private lateinit var headers: Map<String, List<String>>
 
   override fun append(data: ByteArray, size: Int) {
-    if (dataSize + size > dataBuffer.size) {
-      dataBuffer = dataBuffer.copyOf(dataSize + size * 4)
+    when (state) {
+      HEADER -> {
+        appendToBuffer(data, size)
+
+        val separatorIndex = dataBuffer.indexOfHeaderBodySeparator(dataSize)
+        if (separatorIndex == -1) return
+
+        val headerLines = dataBuffer.decodeToString(endIndex = separatorIndex).split("\r\n")
+
+        val statusLine = headerLines.first()
+        check(statusLine.startsWith("HTTP/1.1")) { "Expected HTTP/1.1 in response." }
+        statusCode = statusLine.split(' ', limit = 3)[1].toInt()
+
+        headers = headerLines.subList(1, headerLines.size)
+          .map { it.split(": ", limit = 2) }
+          .groupBy({ it[0] }, { it[1] })
+
+        if (headers["Transfer-Encoding"]?.get(0) == "chunked") {
+          dataBuffer = dataBuffer.copyInto(dataBuffer, 0, separatorIndex + 4, dataSize)
+          dataSize = dataSize - separatorIndex - 4
+          state = if (readChunksToBuffer()) DONE else CHUNKS
+        } else {
+          contentLength = checkNotNull(headers["Content-Length"]?.get(0)?.toInt()) { "Expected Content-Length header." }
+
+          dataBuffer = dataBuffer.copyInto(ByteArray(contentLength), 0, separatorIndex + 4, dataSize)
+          dataSize = dataSize - separatorIndex - 4
+          state = if (dataSize == contentLength) DONE else BODY
+        }
+      }
+
+      BODY -> {
+        appendToBuffer(data, size)
+        if (dataSize == contentLength) state = DONE
+      }
+
+      CHUNKS -> {
+        appendToBuffer(data, size)
+
+        // Check if last 5 bytes are the last chunk, 0\r\n\r\n
+        if (
+          dataSize >= 5 && dataBuffer[dataSize - 5] == 48.toByte() && dataBuffer[dataSize - 4] == 13.toByte() &&
+          dataBuffer[dataSize - 3] == 10.toByte() && dataBuffer[dataSize - 2] == 13.toByte() &&
+          dataBuffer[dataSize - 1] == 10.toByte()
+        ) {
+          if (readChunksToBuffer()) state = DONE
+        }
+      }
+
+      DONE -> error("Response builder is already in done state.")
     }
-    data.copyInto(dataBuffer, dataSize, endIndex = size)
+  }
+
+  fun build(): HttpResponse = HttpResponse(dataBuffer, headers, statusCode)
+
+  override fun completed() = state == DONE
+
+  private fun readChunksToBuffer(): Boolean {
+    val data = ByteArray(dataSize)
+    var dataIndex = 0
+    var readIndex = 0
+    while (readIndex < dataSize) {
+      val line = dataBuffer.readLine(readIndex, dataSize) ?: return false
+      readIndex += line.length + 2
+
+      val chunkSize = line.substringBefore(';').toInt(16)
+      if (chunkSize == 0) {
+        dataSize = dataIndex
+        dataBuffer = data.copyInto(ByteArray(dataIndex), endIndex = dataIndex)
+        return true
+      }
+
+      dataBuffer.copyInto(data, startIndex = readIndex, endIndex = readIndex + chunkSize, destinationOffset = dataIndex)
+      readIndex += chunkSize + 2
+      dataIndex += chunkSize
+    }
+
+    return false
+  }
+
+  private fun appendToBuffer(data: ByteArray, size: Int) {
+    if (dataSize + size > dataBuffer.size) dataBuffer = dataBuffer.copyOf(dataSize + size * 4)
+    data.copyInto(this.dataBuffer, this.dataSize, endIndex = size)
     dataSize += size
-
-    if (statusCode != -1) return
-    val separatorIndex = dataBuffer.indexOfHeaderBodySeparator(dataSize)
-    if (separatorIndex == -1) return
-
-    val headerLines = dataBuffer.decodeToString(endIndex = separatorIndex).split("\r\n")
-
-    val statusLine = headerLines.first()
-    check(statusLine.startsWith("HTTP/1.1")) { "Expected HTTP/1.1 in response." }
-    statusCode = statusLine.split(' ', limit = 3)[1].toInt()
-
-    headers = headerLines.subList(1, headerLines.size)
-      .map { it.split(": ", limit = 2) }
-      .groupBy({ it[0] }, { it[1] })
-
-    contentLength = checkNotNull(headers["Content-Length"]?.get(0)?.toInt()) { "Expected Content-Length header." }
-
-    dataBuffer = dataBuffer.copyInto(ByteArray(contentLength), startIndex = separatorIndex + 4, endIndex = dataSize)
-    dataSize = dataSize - separatorIndex - 4
   }
 
-  fun build(): HttpResponse {
-    return HttpResponse(dataBuffer, headers, statusCode)
+  private fun ByteArray.readLine(start: Int, end: Int): String? {
+    for (i in start until end - 1) {
+      if (this[i] == 13.toByte() && this[i + 1] == 10.toByte()) return decodeToString(start, i)
+    }
+    return null
   }
 
-  override fun completed() = statusCode != -1 && dataSize == contentLength
-}
+  private enum class HttpResponseState {
+    HEADER,
+    BODY,
+    CHUNKS,
+    DONE
+  }
 
-private tailrec fun ByteArray.indexOfHeaderBodySeparator(size: Int, start: Int = 0): Int {
-  if (start + 4 > size) return -1
-  if (
-    this[start] == 13.toByte() && this[start + 1] == 10.toByte() &&
-    this[start + 2] == 13.toByte() && this[start + 3] == 10.toByte()
-  ) return start
-  return this.indexOfHeaderBodySeparator(size, start + 1)
+  private tailrec fun ByteArray.indexOfHeaderBodySeparator(size: Int, start: Int = 0): Int {
+    if (start + 4 > size) return -1
+    if (
+      this[start] == 13.toByte() && this[start + 1] == 10.toByte() &&
+      this[start + 2] == 13.toByte() && this[start + 3] == 10.toByte()
+    ) return start
+    return this.indexOfHeaderBodySeparator(size, start + 1)
+  }
 }
--- runtime/src/commonTest/kotlin/com/asyncant/http/HttpResponseBuilderTest.kt
+++ runtime/src/commonTest/kotlin/com/asyncant/http/HttpResponseBuilderTest.kt
@@ -83,4 +83,128 @@ internal class HttpResponseBuilderTest {
     val expectedHeaders = expected.headers + ("Content-Length" to listOf(body.length.toString()))
     assertEquals(expected.copy(body.encodeToByteArray(), expectedHeaders), builder.build())
   }
+
+  @Test
+  fun chunkedBody() {
+    val builder = HttpResponseBuilder()
+
+    val headers =
+      "HTTP/1.1 200 OK\r\n" +
+        "Content-Type: application/json\r\n" +
+        "Lambda-Runtime-Aws-Request-Id: abc-123-def\r\n" +
+        "Lambda-Runtime-Deadline-Ms: 123\r\n" +
+        "Lambda-Runtime-Invoked-Function-Arn: arn:aws:lambda:eu-central-1:123:function:kotlin-native-hello-world\r\n" +
+        "Lambda-Runtime-Trace-Id: Root=1-123-abc;Parent=456;Sampled=0\r\n" +
+        "Date: Wed, 10 Aug 2021 20:00:00 GMT\r\n" +
+        "Transfer-Encoding: chunked\r\n" +
+        "\r\n"
+
+    // When the headers are sent and the builder is instructed of the chunked transfer-encoding
+    builder.append(headers.encodeToByteArray(), headers.length)
+    // then the builder is not complete yet.
+    assertFalse(builder.completed())
+
+    // When the first body chunk is sent
+    val firstChunk = "5\r\nHello\r\n".encodeToByteArray()
+    builder.append(firstChunk, firstChunk.size)
+    // then the builder is not complete yet.
+    assertFalse(builder.completed())
+
+    // When the second body chunk is sent
+    val secondChunk = "d\r\n world! Test!\r\n".encodeToByteArray()
+    builder.append(secondChunk, secondChunk.size)
+    // then the builder is not complete yet.
+    assertFalse(builder.completed())
+
+    // When the last body chunk, an empty chunk, is sent
+    val lastChunk = "0\r\n\r\n".encodeToByteArray()
+    builder.append(lastChunk, lastChunk.size)
+    // then the builder is complete
+    assertTrue(builder.completed())
+
+    // and a filled in HttpResponse is returned.
+    val expectedHeaders = expected.headers + ("Transfer-Encoding" to listOf("chunked")) - "Content-Length"
+    val expectedBody = "Hello world! Test!".encodeToByteArray()
+    assertEquals(HttpResponse(expectedBody, expectedHeaders, 200), builder.build())
+  }
+
+  @Test
+  fun chunkedBodyPartialChunks() {
+    val builder = HttpResponseBuilder()
+
+    val headers =
+      "HTTP/1.1 200 OK\r\n" +
+        "Content-Type: application/json\r\n" +
+        "Lambda-Runtime-Aws-Request-Id: abc-123-def\r\n" +
+        "Lambda-Runtime-Deadline-Ms: 123\r\n" +
+        "Lambda-Runtime-Invoked-Function-Arn: arn:aws:lambda:eu-central-1:123:function:kotlin-native-hello-world\r\n" +
+        "Lambda-Runtime-Trace-Id: Root=1-123-abc;Parent=456;Sampled=0\r\n" +
+        "Date: Wed, 10 Aug 2021 20:00:00 GMT\r\n" +
+        "Transfer-Encoding: chunked\r\n" +
+        "\r\n"
+
+    // When the headers are sent and the builder is instructed of the chunked transfer-encoding
+    builder.append(headers.encodeToByteArray(), headers.length)
+    // then the builder is not complete yet.
+    assertFalse(builder.completed())
+
+    // When the first body chunk is sent
+    val firstChunkFirstPart = "5\r\nHel".encodeToByteArray()
+    builder.append(firstChunkFirstPart, firstChunkFirstPart.size)
+    val firstChunkSecondPart = "lo\r\n".encodeToByteArray()
+    builder.append(firstChunkSecondPart, firstChunkSecondPart.size)
+    // then the builder is not complete yet.
+    assertFalse(builder.completed())
+
+    // When the second body chunk is sent
+    val secondChunk = "d\r\n world! Test!\r\n".encodeToByteArray()
+    builder.append(secondChunk, secondChunk.size)
+    // then the builder is not complete yet.
+    assertFalse(builder.completed())
+
+    // When the first part of the last body chunk, an empty chunk, is sent
+    val lastChunkFirstPart = "0\r\n\r".encodeToByteArray()
+    builder.append(lastChunkFirstPart, lastChunkFirstPart.size)
+    // then the builder is not complete yet
+    assertFalse(builder.completed())
+    // and after the last part of the last body chunk, an empty chunk, is sent
+    val lastChunkSecondPart = "\n".encodeToByteArray()
+    builder.append(lastChunkSecondPart, lastChunkSecondPart.size)
+    // then the builder is complete
+    assertTrue(builder.completed())
+
+    // and a filled in HttpResponse is returned.
+    val expectedHeaders = expected.headers + ("Transfer-Encoding" to listOf("chunked")) - "Content-Length"
+    val expectedBody = "Hello world! Test!".encodeToByteArray()
+    assertEquals(HttpResponse(expectedBody, expectedHeaders, 200), builder.build())
+  }
+
+  @Test
+  fun chunkedBodyAsOneResponse() {
+    val builder = HttpResponseBuilder()
+
+    val headers =
+      "HTTP/1.1 200 OK\r\n" +
+        "Content-Type: application/json\r\n" +
+        "Lambda-Runtime-Aws-Request-Id: abc-123-def\r\n" +
+        "Lambda-Runtime-Deadline-Ms: 123\r\n" +
+        "Lambda-Runtime-Invoked-Function-Arn: arn:aws:lambda:eu-central-1:123:function:kotlin-native-hello-world\r\n" +
+        "Lambda-Runtime-Trace-Id: Root=1-123-abc;Parent=456;Sampled=0\r\n" +
+        "Date: Wed, 10 Aug 2021 20:00:00 GMT\r\n" +
+        "Transfer-Encoding: chunked\r\n" +
+        "\r\n" +
+        "5\r\nHello\r\n" +
+        "d\r\n world! Test!\r\n" +
+        "0\r\n\r\n"
+
+    // When the headers and all chunks are sent at once even though the transfer-encoding is chunked
+    builder.append(headers.encodeToByteArray(), headers.length)
+    // then the builder is complete
+    assertTrue(builder.completed())
+
+    // and a filled in HttpResponse is returned.
+    val expectedHeaders = expected.headers + ("Transfer-Encoding" to listOf("chunked")) - "Content-Length"
+    val expectedBody = "Hello world! Test!".encodeToByteArray()
+    assertEquals(HttpResponse(expectedBody, expectedHeaders, 200), builder.build())
+  }
 }
