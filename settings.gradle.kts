92775ec1a18e0ccbeb6c2d98035a3c90b53b4ee6
asyncant <git@asyncant.com>
2025-05-31 19:32:16 +0200

    Update to kotlinx-serialization-json v1.8.1.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,14 @@
 include("runtime", "samples:hello-world", "samples:api-gateway", "samples:lambda-function-urls")
 
-enableFeaturePreview("VERSION_CATALOGS")
 dependencyResolutionManagement {
+  @Suppress("UnstableApiUsage")
   repositories {
     mavenCentral()
   }
 
   versionCatalogs {
     create("libs") {
-      library("kotlinserialization", "org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+      library("kotlinserialization", "org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
     }
   }
 }
