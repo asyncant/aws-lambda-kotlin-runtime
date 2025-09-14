6f97730f173c1b6a3f2de15c0ec5140aacaaa46d
asyncant <git@asyncant.com>
2025-09-14 17:08:44 +0200

    Replace patchelf with linker flags to remove libcrypt dependency.

--- samples/hello-world-al2023/build-lambda.sh
+++ samples/hello-world-al2023/build-lambda.sh
@@ -11,15 +11,6 @@ rm "${script_dir}/build/lambda/lambda.zip" > /dev/null 2>&1 || true
 
 cp "${script_dir}/build/bin/linuxX64/releaseExecutable/hello-world-al2023.kexe" "${script_dir}/build/lambda/bootstrap"
 
-# Remove Kotlin's unused dependency on libcrypt as it is not present on Amazon Linux 2023.
-# * https://youtrack.jetbrains.com/issue/KT-55643
-patchelf --remove-needed libcrypt.so.1 "${script_dir}/build/lambda/bootstrap"
-
-# Alternatively, one can copy the required library from Kotlin's konan distribution:
-# cp "${HOME}/.konan/dependencies/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2/x86_64-unknown-linux-gnu/sysroot/lib/libcrypt-2.19.so" "${script_dir}/build/lambda/libcrypt.so.1"
-# and add it to the zip:
-# (cd "${script_dir}/build/lambda" && zip -qoX lambda.zip bootstrap libcrypt.so.1)
-
 # Create reproducible zip.
 touch -t 200001010000 "${script_dir}/build/lambda/bootstrap"
 (cd "${script_dir}/build/lambda" && zip -qoX lambda.zip bootstrap)
