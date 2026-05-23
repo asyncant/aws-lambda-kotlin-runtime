3d89037cf2cf00d7262430340863bc437d8ce01c
asyncant <git@asyncant.com>
2026-05-23 12:00:18 +0200

    Release version 0.9.6.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
   kotlin("multiplatform") version "2.3.21" apply false
 }
 group = "com.asyncant.aws.lambda"
-version = "0.9.5"
+version = "0.9.6"
 
 allprojects {
   tasks.withType<Test> {
